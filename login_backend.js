75e4552faea22df5b8cb2baf1629be1991c33322
inglomi <92704810+inglomi@users.noreply.github.com>
2023-10-13 18:49:40 -0500

    Update login_backend.js
    revision #1

--- login_backend.js
+++ login_backend.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// parses POST data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -11,11 +10,9 @@ const users = [
   { username: 'user2', password: 'pass2' }
 ];
 
-// POST /login
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // validate input
   if (!username || !password) {
     return res.status(400).send('Missing username or password');
   }
@@ -24,14 +21,12 @@ app.post('/login', (req, res) => {
     return res.status(400).send('Username or password too long');
   }
 
-  // check if user exists
   const user = users.find(user => user.username === username && user.password === password);
 
   if (!user) {
     return res.status(401).send('Invalid username or password');
   }
 
-  // user logged in successfully
   res.send('Login successful');
 });
 
