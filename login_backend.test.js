b5c7bd1ff5d238d427877a47addf8adbe57a0ee1
inglomi <92704810+inglomi@users.noreply.github.com>
2023-10-13 18:51:11 -0500

    Update login_backend.test.js
    revision #1

--- login_backend.test.js
+++ login_backend.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const server = require('../login_backend'); // Assuming your backend file is named login_backend.js
 const should = chai.should();
 
 chai.use(chaiHttp);
