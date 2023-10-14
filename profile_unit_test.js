ffddacf18a3c8e9534dee5ad668c245bc0ec3644
Viet Pham <105752177+LePoisson104@users.noreply.github.com>
2023-10-13 20:29:21 -0500

    Update profile_unit_test.js

--- profile_unit_test.js
+++ profile_unit_test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 
-const app = require('./your-express-app'); // Replace with the path to your Express app
+const server = require('./profile_back_end'); 
 
 chai.use(chaiHttp);
 
@@ -18,13 +18,13 @@ describe('User Profile Update', () => {
       zipcode: '77004',
     };
 
-    const response = await chai.request(app)
-      .post('/submit')
+    const response = await chai.request(server)
+      .post('/profile_saved')
       .send(updateData);
 
     expect(response).to.have.status(200); 
 
     // Now, check if the userProfile object has been updated
-    expect(app.userProfile).to.deep.equal(updateData);
+    expect(server.userProfile).to.deep.equal(updateData);
   });
 });
