dc28053720b2dcfc1bb46e22344cfd3de3f65c0c
Viet Pham <105752177+LePoisson104@users.noreply.github.com>
2023-10-27 21:14:37 -0500

    Update profile_backend.js

--- backend/profile_backend.js
+++ backend/profile_backend.js
@@ -1,66 +1,56 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
+const port = 3000;
+const connection = require('./database');
 
-// Middleware to parse JSON and urlencoded request bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static('client'));
 
-// Serve static files (HTML, CSS, JS)
-app.use(express.static('public'));
-
-// Endpoint to handle form submissions for Personal Information
-app.post('/savePersonalInfo', (req, res) => {
-    const { first_name, last_name, address_1, address_2, city, state, zipcode } = req.body;
-
-    // Validate the data
-    if (!first_name || !last_name || !address_1 || !city || !state || !zipcode) {
-        return res.status(400).json({ error: 'All fields are required.' });
-    }
-
-    // Additional validation logic (e.g., length, format)
-
-    // Prepare data for future DB integration
-    const userData = {
-        first_name,
-        last_name,
-        address_1,
-        address_2,
-        city,
-        state,
-        zipcode
-    };
-
-    // Save userData to DB (will be implemented later)
-
-    res.json({ message: 'Data saved successfully.' });
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('database connected successfully!');
 });
 
-// Endpoint to handle password reset
-app.post('/resetPassword', (req, res) => {
-    const { current_password, new_password, confirm_password } = req.body;
+// //update
+app.patch('/update_info/:clientID', (req, res) => {
+    const uID = req.params.clientID;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const address1 = req.body.address1;
+    const city = req.body.city;
+    const state = req.body.state;
+    const zipcode = req.body.zipcode;
 
-    // Validate the data
-    if (!current_password || !new_password || !confirm_password) {
-        return res.status(400).json({ error: 'All fields are required.' });
-    }
-
-    // Additional validation logic (e.g., password strength)
-
-    // Prepare data for future DB integration (if needed)
-    const passwordData = {
-        current_password,
-        new_password,
-        confirm_password
-    };
-
-    // Reset password logic (will be implemented later)
-
-    res.json({ message: 'Password reset successful.' });
+    connection.query('UPDATE client SET first_name=?, last_name=?, address1=?, city=?, state=?, zipcode=? WHERE clientID=?', 
+    [first_name,last_name,address1,city,state,zipcode,uID],(err, result) =>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send('updated!');
+            console.log(result);
+        }
+    });
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.patch('/update_login/:login_id', (req, res) => {
+    const uID = req.params.login_id;
+    const L_password = req.body.L_password;
+
+    connection.query('UPDATE userLogin SET L_password=? WHERE login_id=?', [L_password,uID],(err, result) =>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.send('updated!');
+            console.log(result);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log('server connected successfully!');
 });
