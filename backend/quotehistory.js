04bdac1dc04eb7d8127c125886d4911f67837c0f
tdas-cmd <126375294+tdas-cmd@users.noreply.github.com>
2023-11-29 02:54:35 -0600

    Update quotehistory.js

--- backend/quotehistory.js
+++ backend/quotehistory.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 // Create a connection to the database
 const connection = mysql.createConnection({
     host : "db4free.net",
-    port: '3306',
     database : 'fuelquoteproject',
     user : 'team5fuelquote',
-    password : '1caddf1a'
+    password : '1caddf1a',
+    secret: '8fa1dee245a27cc3c2e12c58634247df932b3582e79baa3f55430ce156999027c434b11e472681f1d134d932b42a37a45c8e394ce744dfa5ed475c2497990c23'
 });
 
 // Connect to the MySQL server
@@ -32,29 +32,30 @@ function generateTable(data){
   html += '</table>';
   return html;
 }
-app.get('/', (req, res) => {
+app.get('/sendData', (req, res) => {
     // Fetch data from the database
-    connection.query('SELECT * FROM your_table_name', (error, results, fields) => {
+    connection.query('SELECT * FROM FuelQuotes', (error, results, fields) => {
         if (error) throw error;
 
         const qoute = results.map(row => {
-            const name = row.fullname;
-            const date = row.qouteDate;
-            const gallon_requested = row.gallonRequested;
-            const address = row.Address1;
-            if (row.Address2 == ''){
-                address = row.Address2 + ","+row.Address1;
-            }
-            const price = suggestedPrice;
-            const state = row.City + ", "+row.state;
-            const zipcode = row.Zipcode;
-            const price_per_gallon = price / gallon_requested ;
-            const deliverydate = row.deliveryDate;
+          let qouteID = row.quoteID;
+          let gallon_requested = row.gallons;
+          let address = row.address;
+          if (row.secondAddress != ''){
+            address = row.secondAddress + ","+row.address;
+          }
+          let state = row.city + ", "+row.state;
+          let zipcode = row.zip;
+          let price_per_gallon = row.price;
+          let total = row.total;
+          let date = row.deliveryDate;
+          let deliverydate = date.toDateString();
       
-            return {name, date,gallon_requested,address, state, zipcode, price_per_gallon, deliverydate}; 
+          return {qouteID,gallon_requested,address, state, zipcode, price_per_gallon, total,deliverydate}; 
           });
-      // Pass the data to the HTML template
-      res.send(`<html><body>${generateTable(qoute)}</body></html>`);
+
+      // Pass the data to the HTML
+      res.json(qoute);
     });
   });
 app.listen(3000, () => {
