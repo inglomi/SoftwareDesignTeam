0fed2d6f5739601e538022e4028f05c706fe3cfe
inglomi <92704810+inglomi@users.noreply.github.com>
2023-12-02 01:35:53 -0600

    Update login_backend.test.js

--- backend/login_backend.test.js
+++ backend/login_backend.test.js
@@ -1,43 +1,134 @@
 const request = require('supertest');
 const app = require('../server');
 const chai = require('chai');
-const assert = chai.assert;
-const mocha = require('./node_modules/mocha');
+const expect = chai.expect;
+const faker = require('faker');
 
 describe('Login backend routes', () => {
   it('should return login successful for valid credentials', async () => {
+    // generate random user with valid credentials
+    const user = {
+      username: faker.internet.userName(),
+      password: faker.internet.password()
+    };
+
+    // register user in database
+    await request(app)
+      .post('/registrationForm')
+      .send(user);
+
+    // login with same credentials
     const response = await request(app)
       .post('/loginForm')
-      .send({ username: 'user1', password: 'pass1' });
+      .send(user);
     
-    assert.equal(response.statusCode, 200);
-    assert.equal(response.text, 'Login successful');
+    expect(response.statusCode).to.equal(200);
+    expect(response.text).to.equal('Login successful');
   });
 
   it('should return invalid username or password for invalid credentials', async () => {
+    // generate random user with invalid credentials
+    const user = {
+      username: faker.internet.userName(),
+      password: faker.internet.password()
+    };
+
+    // login with invalid credentials
     const response = await request(app)
       .post('/loginForm')
-      .send({ username: 'user1', password: 'wrong' });
+      .send(user);
     
-    assert.equal(response.statusCode, 401);
-    assert.equal(response.text, 'Invalid username or password');
+    expect(response.statusCode).to.equal(401);
+    expect(response.text).to.equal('Invalid username or password');
   });
 
   it('should return missing username or password for empty credentials', async () => {
+    // login with empty credentials
     const response = await request(app)
       .post('/loginForm')
       .send({ username: '', password: '' });
     
-    assert.equal(response.statusCode, 400);
-    assert.equal(response.text, 'Missing username or password');
+    expect(response.statusCode).to.equal(400);
+    expect(response.text).to.equal('Missing username or password');
   });
 
   it('should return username or password too long for long credentials', async () => {
+    // generate random user with long credentials
+    const user = {
+      username: faker.internet.userName().repeat(5),
+      password: faker.internet.password().repeat(5)
+    };
+
+    // login with long credentials
     const response = await request(app)
       .post('/loginForm')
-      .send({ username: 'user1'.repeat(5), password: 'pass1'.repeat(5) });
+      .send(user);
     
-    assert.equal(response.statusCode, 400);
-    assert.equal(response.text, 'Username or password too long');
+    expect(response.statusCode).to.equal(400);
+    expect(response.text).to.equal('Username or password too long');
+  });
+});
+
+describe('Registration backend routes', () => {
+  it('should return registration successful for valid credentials', async () => {
+    // generate random user with valid credentials
+    const user = {
+      username: faker.internet.userName(),
+      password: faker.internet.password()
+    };
+
+    // register user in database
+    const response = await request(app)
+      .post('/registrationForm')
+      .send(user);
+
+    expect(response.statusCode).to.equal(200);
+    expect(response.text).to.equal('Registration successful');
+  });
+
+  it('should return username already exists for duplicate credentials', async () => {
+    // generate random user with valid credentials
+    const user = {
+      username: faker.internet.userName(),
+      password: faker.internet.password()
+    };
+
+    // register user in database twice
+    await request(app)
+      .post('/registrationForm')
+      .send(user);
+
+    const response = await request(app)
+      .post('/registrationForm')
+      .send(user);
+
+    expect(response.statusCode).to.equal(409);
+    expect(response.text).to.equal('Username already exists');
+  });
+
+  it('should return missing username or password for empty credentials', async () => {
+    // register with empty credentials
+    const response = await request(app)
+      .post('/registrationForm')
+      .send({ username: '', password: '' });
+
+    expect(response.statusCode).to.equal(400);
+    expect(response.text).to.equal('Missing username or password');
+  });
+
+  it('should return username or password too long for long credentials', async () => {
+    // generate random user with long credentials
+    const user = {
+      username: faker.internet.userName().repeat(5),
+      password: faker.internet.password().repeat(5)
+    };
+
+    // register with long credentials
+    const response = await request(app)
+      .post('/registrationForm')
+      .send(user);
+
+    expect(response.statusCode).to.equal(400);
+    expect(response.text).to.equal('Username or password too long');
   });
 });
