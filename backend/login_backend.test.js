7017874f3ad1a64630810f4cbcbe3e0b5bdc6c80
inglomi <92704810+inglomi@users.noreply.github.com>
2023-12-02 01:06:31 -0600

    Update login_backend.test.js

--- backend/login_backend.test.js
+++ backend/login_backend.test.js
@@ -1,36 +1,43 @@
 const request = require('supertest');
 const app = require('../server');
+const chai = require('chai');
+const assert = chai.assert;
+const mocha = require('./node_modules/mocha');
 
-describe('Login backend routes', () => {
-  test('POST /loginForm should return login successful for valid credentials', async () => {
+mocha.describe('Login backend routes', () => {
+  mocha.it('should return login successful for valid credentials', async () => {
     const response = await request(app)
       .post('/loginForm')
       .send({ username: 'user1', password: 'pass1' });
-    expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Login successful');
+    
+    assert.equal(response.statusCode, 200);
+    assert.equal(response.text, 'Login successful');
   });
 
-  test('POST /loginForm should return invalid username or password for invalid credentials', async () => {
+  mocha.it('should return invalid username or password for invalid credentials', async () => {
     const response = await request(app)
       .post('/loginForm')
       .send({ username: 'user1', password: 'wrong' });
-    expect(response.statusCode).toBe(401);
-    expect(response.text).toBe('Invalid username or password');
+    
+    assert.equal(response.statusCode, 401);
+    assert.equal(response.text, 'Invalid username or password');
   });
 
-  test('POST /loginForm should return missing username or password for empty credentials', async () => {
+  mocha.it('should return missing username or password for empty credentials', async () => {
     const response = await request(app)
       .post('/loginForm')
       .send({ username: '', password: '' });
-    expect(response.statusCode).toBe(400);
-    expect(response.text).toBe('Missing username or password');
+    
+    assert.equal(response.statusCode, 400);
+    assert.equal(response.text, 'Missing username or password');
   });
 
-  test('POST /loginForm should return username or password too long for long credentials', async () => {
+  mocha.it('should return username or password too long for long credentials', async () => {
     const response = await request(app)
       .post('/loginForm')
       .send({ username: 'user1'.repeat(5), password: 'pass1'.repeat(5) });
-    expect(response.statusCode).toBe(400);
-    expect(response.text).toBe('Username or password too long');
+    
+    assert.equal(response.statusCode, 400);
+    assert.equal(response.text, 'Username or password too long');
   });
 });
